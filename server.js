e777aa748c3b3d67f770e0df4eab8f47389d8d6b
daniel_eichinger <d.eichinger@campus.lmu.de>
2019-05-18 14:32:41 +1000

    adds production scripts

--- server.js
+++ server.js
@@ -10,11 +10,24 @@ app.use(bodyParser.json());
 // Check databases
 databaseChecker.checkDatabases();
 
-
 // Set-Up Routes
 app.use('/api/devices', devicesApi);
 
 
+// parse node arguments
+const arguments = process.argv;
+if (arguments.includes("production")) {
+    console.info("PRODUCTION--SERVING /client/build\n\n");
+
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+
+// Run Server
 const port = 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
