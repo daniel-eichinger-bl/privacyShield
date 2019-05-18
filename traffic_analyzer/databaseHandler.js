54247698b8df4f63f1ec1a329ce0976943082be6
daniel_eichinger <d.eichinger@campus.lmu.de>
2019-05-18 11:21:10 +1000

    adds pushing of packets to database

--- traffic_analyzer/databaseHandler.js
+++ traffic_analyzer/databaseHandler.js
@@ -1,27 +1,38 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// TODO
-exports.setupDatabse = () => {
-    /* Database Connection */
-    const db = new sqlite3.cached.Database('../data/database', err => {
-        if (err === null) {
-            console.log("\nDatabase Connection successfull")
-        } else {
-            console.error("\nError - Database Connection")
-        }
-    });
+/* Database Connection */
+const db = new sqlite3.cached.Database('../data/database', err => {
+    if (err === null) {
+        console.log("\nDatabase Connection successfull")
+    } else {
+        console.error("\nError - Database Connection")
+    }
+});
 
+exports.setupDatabse = () => {
     /* Check if database table exits and creates it */
     db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='privacy_traffic'", (err, row) => {
         if (!row) {
             console.log("--> PrivacyTraffic Database created!")
-            db.run("CREATE TABLE privacy_traffic (mac TEXT PRIMARY KEY, timestamp INTEGER, ip TEXT, blocked INTEGER DEFAULT 0, name TEXT)", err => {
+            db.run("CREATE TABLE privacy_traffic (macSrc TEXT, macDest TEXT, timestamp INTEGER, type TEXT, host TEXT)", err => {
                 if (err) {
                     console.error(err);
                 }
             });
         } else {
             console.log("--> PrivacyTraffic Database found!")
+        }
+    });
+
+}
+
+exports.addToDb = (packet) => {
+    const { macSrc, macDest, timestamp, type, host } = packet;
+
+    const stmt = "INSERT INTO privacy_traffic(macSrc, macDest, timestamp, type, host) VALUES (?,?,?,?,?)";
+    db.get(stmt, [macSrc, macDest, timestamp, type, host], (err, row) => {
+        if (err) {
+            console.log(err);
         }
     });
 
--- traffic_analyzer/index.js
+++ traffic_analyzer/index.js
@@ -2,76 +2,88 @@ const pcap = require('pcap');
 const tlsClientHello = require('is-tls-client-hello');
 const sni = require('sni');
 const parser = require('./httpPayloadParser');
+const databaseHandler = require('./databaseHandler');
 
-const pcap_session = pcap.createSession('br0', "ip proto \\tcp");
 
-const parseMac = (e) => {
-    const byte = e.toString(16);
-    return byte.length === 1 ? `0${byte}` : byte;
+
+databaseHandler.setupDatabse();
+setTimeout(main, 3000);
+
+function main() {
+    const pcap_session = pcap.createSession('br0', "ip proto \\tcp");
+
+    const parseMac = (e) => {
+        const byte = e.toString(16);
+        return byte.length === 1 ? `0${byte}` : byte;
+    }
+
+    pcap_session.on('packet', function (raw_packet) {
+        try {
+            const packet = pcap.decode.packet(raw_packet);
+
+            const timestamp = packet.pcap_header.tv_sec;
+            const eth = packet.payload;
+            const ip = eth.payload;
+
+            if (!ip) {
+                return;
+            }
+
+            const tcp = ip.payload;
+
+            if (ip.protocolName === 'Unknown' || typeof ip.payload === 'undefined') {
+                return;
+            }
+
+            const macSrc = eth.shost.addr.map(parseMac).join(':').toUpperCase();
+            const macDest = eth.dhost.addr.map(parseMac).join(':').toUpperCase();
+
+            const src = ip.saddr.addr.join('.')
+            const dst = ip.daddr.addr.join('.')
+
+            if (!tcp.data) {
+                return;
+            }
+
+
+            if (tcp.sport === 8443 || tcp.sport === 443 || tcp.dport === 443 || tcp.dport === 8443) {
+                if (tlsClientHello(tcp.data)) {
+                    const url = sni(tcp.data);
+                    //const obj = { ts: timestamp, shost: macSrc, dhost: macDest, saddr: src, daddr: dst, sport: tcp.sport, dport: tcp.dport, type: 'https', host: url }
+                    //console.log({ obj });
+
+                    const data = { macSrc, macDest, timestamp, type: "https", host: url }
+                    console.log(data);
+                    databaseHandler.addToDb(data);
+                    return;
+                }
+            }
+
+            const r = tcp.data.toString('utf-8')
+            if (r.indexOf('Content-Length') === -1 &&
+                r.indexOf('Host') === -1 &&
+                r.indexOf('Content-Type') === -1) {
+                return;
+            }
+
+            const host = parser.parseHTTPPayload(r);
+
+            if (host === "") {
+                return;
+            }
+
+            //const obj = { ts: timestamp, shost: macSrc, dhost: macDest, saddr: src, daddr: dst, sport: tcp.sport, dport: tcp.dport, type: 'http', host };
+            //console.log({ obj });
+
+            const data = { macSrc, macDest, timestamp, type: "http", host }
+            console.log(data);
+            databaseHandler.addToDb(data);
+        } catch (err) {
+            console.error(err);
+        }
+    });
 }
 
-pcap_session.on('packet', function (raw_packet) {
-    try {
-    const packet = pcap.decode.packet(raw_packet);
-
-    const ts = packet.pcap_header.tv_sec;
-    const eth = packet.payload;
-    const ip = eth.payload;
-
-    if (!ip) {
-        return;
-    }
-
-    const tcp = ip.payload;
-
-    if (ip.protocolName === 'Unknown' || typeof ip.payload === 'undefined') {
-        return;
-    }
-
-    const shost = eth.shost.addr.map(parseMac).join(':').toUpperCase();
-    const dhost = eth.dhost.addr.map(parseMac).join(':').toUpperCase();
-
-    const src = ip.saddr.addr.join('.')
-    const dst = ip.daddr.addr.join('.')
-
-    if (!tcp.data) {
-        return;
-    }
-
-
-    if (tcp.sport === 8443 || tcp.sport === 443 || tcp.dport === 443 || tcp.dport === 8443) { 
-        if (tlsClientHello(tcp.data)) {
-            const url = sni(tcp.data);
-            const obj = {ts: ts, shost: shost , dhost: dhost, saddr: src, daddr: dst, sport: tcp.sport, dport: tcp.dport, type: 'https', host: url}
-            console.log({obj});
-            return;
-        }
-    }
-
-
-
-    const r = tcp.data.toString('utf-8')
-    if (r.indexOf('Content-Length') === -1 &&
-        r.indexOf('Host') === -1 &&
-        r.indexOf('Content-Type') === -1) {
-        return;
-    }
-
-    const host = parser.parseHTTPPayload(r);
-
-    if(host === "") {
-        return;
-    }
-
-    const obj = { ts: ts, shost: shost, dhost: dhost, saddr: src, daddr: dst, sport: tcp.sport, dport: tcp.dport, type: 'http', host };
-    console.log({obj});
-    } catch (err) {
-        console.error(err);
-    }
-});
-
-
-/* 
+/*
 Possible to use TCP tracker and then see whats requested in a session
-
 */
